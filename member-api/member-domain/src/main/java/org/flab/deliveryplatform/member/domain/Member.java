cca3fbaf338acffc248d94b6ad313194253049ad
SeunghyunYoo <dysh1017@gmail.com>
2022-06-15 04:59:06 +0900

    [feat] member properties 추가

--- member-api/member-domain/src/main/java/org/flab/deliveryplatform/member/domain/Member.java
+++ member-api/member-domain/src/main/java/org/flab/deliveryplatform/member/domain/Member.java
@@ -1,14 +1,33 @@
 package org.flab.deliveryplatform.member.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * @author SeunghyunYoo
  */
+@Getter
 public class Member {
 
     private Long id;
+
+    private String nickname;
 
     private String email;
 
     private String password;
 
+    private String phoneNumber;
+
+    @Builder
+    public Member(String nickname, String email, String password, String phoneNumber) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
