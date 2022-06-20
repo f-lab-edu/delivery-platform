9e38bc8be3558aa16f19be204489461e204b3f88
SeunghyunYoo <dysh1017@gmail.com>
2022-06-20 18:16:49 +0900

    [refactor] ServerApplication에 scanBasePackage 추가 및 member module import

--- server/src/main/java/org/flab/deliveryplatform/server/ServerApplication.java
+++ server/src/main/java/org/flab/deliveryplatform/server/ServerApplication.java
@@ -3,7 +3,7 @@ package org.flab.deliveryplatform.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.flab.deliveryplatform")
 public class ServerApplication {
 
     public static void main(String[] args) {
