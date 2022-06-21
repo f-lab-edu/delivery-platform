f4abd7e4c251ec7e550cdb9119778ca74c0ee249
SeunghyunYoo <dysh1017@gmail.com>
2022-06-21 23:34:46 +0900

    [refactor] RestControllerCommonExceptionAdvice module 위치 변경 및 dependency 정리

--- server/src/main/java/org/flab/deliveryplatform/server/web/exception/RestControllerCommonExceptionAdvice.java
+++ server/src/main/java/org/flab/deliveryplatform/server/web/exception/RestControllerCommonExceptionAdvice.java
@@ -1,13 +1,16 @@
-package org.flab.deliveryplatform.common.web.exception;
+package org.flab.deliveryplatform.server.web.exception;
 
 import static org.flab.deliveryplatform.common.web.exception.DeliveryPlatformErrorCode.BAD_REQUEST;
 
 import org.flab.deliveryplatform.common.web.dto.DeliveryPlatformErrorResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class RestControllerCommonExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
@@ -17,3 +20,4 @@ public class RestControllerCommonExceptionAdvice {
                 BAD_REQUEST.getMessage()));
     }
 }
+
