4ef1fc320aa3d2de45f8fcbad95d3e8f4aca7088
JamieShin0201 <jamieshin1450@gmail.com>
2022-07-19 10:45:51 +0900

    [fix] `@EqualsAndHashCode` 불필요한 필드 설정 제거

--- shop-api/shop-domain/src/main/java/org/flab/deliveryplatform/shop/domain/Address.java
+++ shop-api/shop-domain/src/main/java/org/flab/deliveryplatform/shop/domain/Address.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@EqualsAndHashCode(of = {"zipCode", "country", "state", "city", "street"})
+@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Address {
