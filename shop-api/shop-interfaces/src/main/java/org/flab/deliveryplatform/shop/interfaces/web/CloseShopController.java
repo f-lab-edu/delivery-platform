3cd185258e5959096f848a4a2c98bf4a0aa35801
JamieShin0201 <jamieshin1450@gmail.com>
2022-07-30 09:18:08 +0900

    [fix] 가게 폐점 endpoint url 변경

--- shop-api/shop-interfaces/src/main/java/org/flab/deliveryplatform/shop/interfaces/web/CloseShopController.java
+++ shop-api/shop-interfaces/src/main/java/org/flab/deliveryplatform/shop/interfaces/web/CloseShopController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.flab.deliveryplatform.common.web.dto.DeliveryPlatformResponse;
 import org.flab.deliveryplatform.shop.application.port.CloseShopUseCase;
 import org.flab.deliveryplatform.shop.interfaces.web.common.ShopRestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RequiredArgsConstructor
 @ShopRestController
@@ -13,7 +14,7 @@ public class CloseShopController {
 
     private final CloseShopUseCase closeShopUseCase;
 
-    @DeleteMapping("/{shopId}")
+    @PutMapping("/{shopId}/close")
     public DeliveryPlatformResponse<Void> closeShop(@PathVariable Long shopId) {
         closeShopUseCase.closeShop(shopId);
         return DeliveryPlatformResponse.ok(null);
