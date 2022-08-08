7c30a0cc15b3537c8f393a07d3d0aad5a0313c2a
SeunghyunYoo <dysh1017@gmail.com>
2022-08-09 01:01:14 +0900

    [refactor] OK response entity 제거

--- owner-api/owner-interfaces/src/main/java/org/flab/deliveryplatform/owner/interfaces/web/LoginOwnerController.java
+++ owner-api/owner-interfaces/src/main/java/org/flab/deliveryplatform/owner/interfaces/web/LoginOwnerController.java
@@ -5,10 +5,8 @@ import org.flab.deliveryplatform.common.auth.AuthorizationData;
 import org.flab.deliveryplatform.common.web.dto.DeliveryPlatformResponse;
 import org.flab.deliveryplatform.owner.application.port.LoginOwnerUseCase;
 import org.flab.deliveryplatform.owner.application.port.dto.LoginOwnerCommand;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @OwnerRestController
 @RequiredArgsConstructor
@@ -17,7 +15,6 @@ public class LoginOwnerController {
     private final LoginOwnerUseCase loginOwnerUseCase;
 
     @PostMapping("/login")
-    @ResponseStatus(code = HttpStatus.OK)
     public DeliveryPlatformResponse<AuthorizationData> login(
         @RequestBody LoginOwnerCommand loginOwnerCommand) {
         return DeliveryPlatformResponse.ok(loginOwnerUseCase.login(loginOwnerCommand));
--- owner-api/owner-interfaces/src/main/java/org/flab/deliveryplatform/owner/interfaces/web/SignUpOwnerController.java
+++ owner-api/owner-interfaces/src/main/java/org/flab/deliveryplatform/owner/interfaces/web/SignUpOwnerController.java
@@ -5,10 +5,8 @@ import org.flab.deliveryplatform.common.web.dto.DeliveryPlatformResponse;
 import org.flab.deliveryplatform.owner.application.port.SignUpOwnerUseCase;
 import org.flab.deliveryplatform.owner.application.port.dto.SignUpOwnerCommand;
 import org.flab.deliveryplatform.owner.application.port.dto.SignUpOwnerResult;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @OwnerRestController
 @RequiredArgsConstructor
@@ -17,7 +15,6 @@ public class SignUpOwnerController {
     private final SignUpOwnerUseCase signUpOwnerUseCase;
 
     @PostMapping("/signUp")
-    @ResponseStatus(code = HttpStatus.OK)
     public DeliveryPlatformResponse<SignUpOwnerResult> signUp(@RequestBody SignUpOwnerCommand signUpOwnerCommand) {
         return DeliveryPlatformResponse.ok(signUpOwnerUseCase.signUp(signUpOwnerCommand));
     }
