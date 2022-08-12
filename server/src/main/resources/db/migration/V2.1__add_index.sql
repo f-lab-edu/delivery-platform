aaceef44559350ec58a4b30a2a48c8a2fb7a3d05
JamieShin0201 <jamieshin1450@gmail.com>
2022-08-12 12:15:00 +0900

    [fix] 네이밍 룰에 맞게 인덱스 명 변경

--- server/src/main/resources/db/migration/V2.1__add_index.sql
+++ server/src/main/resources/db/migration/V2.1__add_index.sql
@@ -1,8 +1,8 @@
 ALTER TABLE menu
-    ADD INDEX menu_idx01 (shop_id);
+    ADD INDEX idx__menu_shop_id (shop_id);
 
 ALTER TABLE option_group
-    ADD INDEX option_group_idx01 (menu_id);
+    ADD INDEX idx__option_group_menu_id (menu_id);
 
 ALTER TABLE options
-    ADD INDEX options_idx01 (option_group_id);
+    ADD INDEX idx__options_option_group_id (option_group_id);
