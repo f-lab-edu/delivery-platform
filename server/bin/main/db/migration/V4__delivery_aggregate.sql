d6cfb8e973a38ef27645426a6ee91414fe35b7f3
SeunghyunYoo <dysh1017@gmail.com>
2022-08-24 21:22:16 +0900

    [chore] bin class file 제거

--- server/bin/main/db/migration/V4__delivery_aggregate.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE delivery
-(
-    delivery_id    BIGINT      NOT NULL AUTO_INCREMENT,
-    order_id       BIGINT      NOT NULL,
-    status         VARCHAR(50) NOT NULL,
-    PRIMARY KEY (delivery_id),
-    INDEX idx__delivery_order_id (order_id)
-) CHARACTER SET utf8mb4;
