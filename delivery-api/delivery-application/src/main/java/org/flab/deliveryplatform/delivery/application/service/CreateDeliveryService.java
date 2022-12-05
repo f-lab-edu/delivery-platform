c6e33eec136af6fef1d355b83191291dc153b7b4
JamieShin0201 <jamieshin1450@gmail.com>
2022-12-06 05:26:40 +0900

    [refactor] 배송 생성시 배차전 상태로 생성되도록 변경

--- delivery-api/delivery-application/src/main/java/org/flab/deliveryplatform/delivery/application/service/CreateDeliveryService.java
+++ delivery-api/delivery-application/src/main/java/org/flab/deliveryplatform/delivery/application/service/CreateDeliveryService.java
@@ -3,7 +3,6 @@ package org.flab.deliveryplatform.delivery.application.service;
 import lombok.RequiredArgsConstructor;
 import org.flab.deliveryplatform.delivery.application.port.CreateDeliveryUseCase;
 import org.flab.deliveryplatform.delivery.application.port.DeliveryRepository;
-import org.flab.deliveryplatform.delivery.application.port.dto.CreateDeliveryCommand;
 import org.flab.deliveryplatform.delivery.domain.Delivery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +15,7 @@ public class CreateDeliveryService implements CreateDeliveryUseCase {
 
     @Transactional
     @Override
-    public void createDelivery(CreateDeliveryCommand command) {
-        Delivery delivery = Delivery.builder()
-            .orderId(command.getOrderId())
-            .status(command.getDeliveryStatus())
-            .build();
-
-        deliveryRepository.save(delivery);
+    public void createDelivery(Long orderId) {
+        deliveryRepository.save(Delivery.of(orderId));
     }
 }
--- delivery-api/delivery-interfaces/src/main/java/org/flab/deliveryplatform/delivery/interfaces/eventhandler/OrderPayedEventHandler.java
+++ delivery-api/delivery-interfaces/src/main/java/org/flab/deliveryplatform/delivery/interfaces/eventhandler/OrderPayedEventHandler.java
@@ -2,8 +2,6 @@ package org.flab.deliveryplatform.delivery.interfaces.eventhandler;
 
 import lombok.RequiredArgsConstructor;
 import org.flab.deliveryplatform.delivery.application.port.CreateDeliveryUseCase;
-import org.flab.deliveryplatform.delivery.application.port.dto.CreateDeliveryCommand;
-import org.flab.deliveryplatform.delivery.domain.DeliveryStatus;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +13,6 @@ public class OrderPayedEventHandler {
 
     @EventListener
     public void handle(OrderPayedApplicationEvent event) {
-        CreateDeliveryCommand command = new CreateDeliveryCommand(event.getOrderId(), DeliveryStatus.DELIVERING);
-        createDeliveryUseCase.createDelivery(command);
+        createDeliveryUseCase.createDelivery(event.getOrderId());
     }
 }
